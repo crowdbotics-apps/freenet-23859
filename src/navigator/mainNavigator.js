da7e936bac3d17a2bb56a7ba35a5a22bbe44abf7
Crowdbotics <team@crowdbotics.com>
2021-01-15 09:23:21 +0000

    remove package blueprint-react-native-tutorial-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import Settings193414Navigator from '../features/Settings193414/navigator';
 import EmailAuth193394Navigator from '../features/EmailAuth193394/navigator';
 import Maps193391Navigator from '../features/Maps193391/navigator';
-import Tutorial193390Navigator from '../features/Tutorial193390/navigator';
 
 /**
  * new navigators can be imported here
@@ -20,7 +19,6 @@ const AppNavigator = {
 Settings193414: { screen: Settings193414Navigator },
 EmailAuth193394: { screen: EmailAuth193394Navigator },
 Maps193391: { screen: Maps193391Navigator },
-Tutorial193390: { screen: Tutorial193390Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
