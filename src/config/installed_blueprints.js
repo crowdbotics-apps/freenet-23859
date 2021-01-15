26b5ccce05f704659d53796453d43e74691099c9
Crowdbotics <team@crowdbotics.com>
2021-01-15 08:36:07 +0000

    add package blueprint-react-native-calendar-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'CalendarView193392', human_name: 'Calendar View', access_route: 'CalendarView193392', icon: 'calendar'},
 { name: 'Maps193391', human_name: 'Maps', access_route: 'Maps193391', icon: 'map'},
 { name: 'Tutorial193390', human_name: 'Tutorial', access_route: 'Tutorial193390', icon: 'question-circle'},
 { name: 'Messaging193389', human_name: 'Messaging', access_route: 'Messaging193389', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import CalendarView193392Navigator from '../features/CalendarView193392/navigator';
 import Maps193391Navigator from '../features/Maps193391/navigator';
 import Tutorial193390Navigator from '../features/Tutorial193390/navigator';
 import Messaging193389Navigator from '../features/Messaging193389/navigator';
@@ -16,6 +17,7 @@ import Messaging193389Navigator from '../features/Messaging193389/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+CalendarView193392: { screen: CalendarView193392Navigator },
 Maps193391: { screen: Maps193391Navigator },
 Tutorial193390: { screen: Tutorial193390Navigator },
 Messaging193389: { screen: Messaging193389Navigator },
